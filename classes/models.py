3702bcc0b40e9923882319bd79d47ac7c0943338
Farah <farahaladwanii24@gmail.com>
2023-03-05 19:07:39 +0300

    done

--- classes/models.py
+++ classes/models.py
@@ -16,6 +16,8 @@ class Lecture(models.Model):
 class Slide(models.Model):
     name = models.CharField(max_length=30)
     link = models.URLField()
+    lecture = models.OneToOneField(Lecture, on_delete=models.CASCADE, 
+        primary_key=True,)
 
     def __str__(self):
         return self.name
